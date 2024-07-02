fcb0b49bd3782e64104206aa75093fb8e0e9e60c
Nandana-p-a <nandanapa8888@gmail.com>
2024-07-02 14:06:54 +0530

    kk

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import React, {useState} from 'react';
+import SignIn from './componets/signin';
 
+
+import { BrowserRouter, Router, Route, Link } from 'react-router-dom';
 function App() {
+    const [location, setLocation] = useState('HARIKRISHNANS');
+
+  const handleUpdateLocation = () => {
+    const newLocation = prompt('Enter your new location:');
+    if (newLocation) {
+      setLocation(newLocation);
+    }
+  };
   return (
+    
     <div className="App">
       {/* <header className="App-header">
-<<<<<<< HEAD
-        <img src={logo} className="App-logo" alt="logo" />
-=======
         <img src={logoo} className="App-logo" alt="logo" />
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,77 +32,42 @@ function App() {
       </header> */}
 <body>
     <header>
-<<<<<<< HEAD
+      <BrowserRouter>
         <div className="container">
             <div className="logo">
-                <img src="D:\Study Materials\1000107008-h.webp" />
-                <img src="https://raw.githubusercontent.com/harikrishnan-ps/images/master/_fad40a76-c679-4c76-8e22-8ec973ec596f.jpeg" />
+                <img src="https://icons.veryicon.com/png/o/miscellaneous/flower-mall-color-icon/shopping-cart-114.png"/>
             </div>
             <div className="location">
                 <p>HARIKRISHNANS</p>
-                <button>Update Location</button>
+                <button >Update Location</button>
             </div>
             <div className="search-bar">
                 <input type="text" placeholder="Search Product" />
                 <button><i className="fas fa-search"></i></button>
             </div>
             <div className="user-actions">
-=======
-        <div class="container">
-            <div class="logo">
-                <img src="https://icons.veryicon.com/png/o/miscellaneous/flower-mall-color-icon/shopping-cart-114.png"/>
-            </div>
-            <div class="location">
-                <p>HARIKRISHNANS</p>
-                <button>Update Location</button>
-            </div>
-            <div class="search-bar">
-                <input type="text" placeholder="Search Product" />
-                <button><i class="fas fa-search"></i></button>
-            </div>
-            <div class="user-actions">
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
                 <button>EN</button>
-                <button>Hello, sign in
-                    Account & Lists ▾</button>
+                <Link to="/signin"><button>Hello, sign in
+                    Account & Lists ▾</button></Link>
                 <button>Returns
                     & Orders</button>
-<<<<<<< HEAD
                 <div className="cart">
                     <i className="fas fa-shopping-cart"></i>
-=======
-                <div class="cart">
-                    <i class="fas fa-shopping-cart"></i>
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
                     <span>0</span>
                 </div>
             </div>
         </div>
+        </BrowserRouter>
     </header>
     <nav>
-<<<<<<< HEAD
         <div className="container">
-=======
-        <div class="container">
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
             <ul>
                 <li><a href="#">All</a></li>
-                <li><a href="#">Fresh</a></li>
-                <li><a href="#">Sell</a></li>
-                <li><a href="#">Best Sellers</a></li>
                 <li><a href="#">Mobiles</a></li>
-                <li><a href="#">Today's Deals</a></li>
                 <li><a href="#">Fashion</a></li>
                 <li><a href="#">Electronics</a></li>
-                <li><a href="#">Customer Service</a></li>
-                <li><a href="#">New Releases</a></li>
-<<<<<<< HEAD
-                <li><a 
-                href="#">Home & Kitchen</a></li>
-=======
                 <li><a href="#">Home & Kitchen</a></li>
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
-                <li><a href="#">Pay</a></li>
+                <li><a href="#" >Pay</a></li>
                 <li><a href="#">Computers</a></li>
                 <li><a href="#">Books</a></li>
             </ul>
@@ -101,116 +75,63 @@ function App() {
     </nav>
 
     <main>
-<<<<<<< HEAD
         <section className="hero">
             <div className="container">
                 <div className="hero-content">
                     <div className="hero-title">
-=======
-        <section class="hero">
-            <div class="container">
-                <div class="hero-content">
-                    <div class="hero-title">
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
                         <h1>Personalised gift store for your loved ones</h1>
                     </div>                   
                 </div>
             </div>
         </section>
-<<<<<<< HEAD
         <section className="categories">
             <div className="container">
                 <div className="category">
                     <h2>Revamp your home in style</h2>
                     <div className="category-items">
                         <div className="category-item">
-                            <img src="D:\Study Materials\NavyBlue_A_650x.webp" />
+                            <img src="https://m.media-amazon.com/images/I/81HbLNd2aqL.AC_UF894,1000_QL80.jpg" />
                             <p>Cushion covers, bedsheets & more</p>
                         </div>
                         <div className="category-item">
-                            <img src="https://github.com/harikrishnan-ps/images/blob/master/612-fQtxDVL.AC_UF1000%2C1000_QL80.jpg" />
-                            <p>Figurines, vases & more</p>
-                        </div>
-                        <div className="category-item">
-                            <img src="D:\Study Materials\shopping.webp" />
-                            <p>Home storage</p>
-                        </div>
-                        <div className="category-item">
-                            <img src="D:\Study Materials\shopping (1).webp" />
-=======
-        <section class="categories">
-            <div class="container">
-                <div class="category">
-                    <h2>Revamp your home in style</h2>
-                    <div class="category-items">
-                        <div class="category-item">
-                            <img src="https://m.media-amazon.com/images/I/81HbLNd2aqL._AC_UF894,1000_QL80_.jpg" />
-                            <p>Cushion covers, bedsheets & more</p>
-                        </div>
-                        <div class="category-item">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzvik-Ylm4GYCK1koyEN4FEfv_y5TCTLZoiA&s" />
                             <p>Figurines, vases & more</p>
                         </div>
-                        <div class="category-item">
+                        <div className="category-item">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL-FBRNNqhfroObYOJ3xbcbrb9qidIubDm_g&s" />
                             <p>Home storage</p>
                         </div>
-                        <div class="category-item">
+                        <div className="category-item">
                             <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTTe8qSoWkh2dXmONO8IhCPiOdUmLBk4A4xGN7a1RdwiDCyUEcNImJ6wcNkptFTfaBxPzZ5S8rK99BF5tWifKK9xQH1OUYWxw&usqp=CAE" />
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
                             <p>Lighting solutions</p>
                         </div>
                     </div>
                     <button>Explore all</button>
                 </div>
-<<<<<<< HEAD
                 <div className="category">
                     <h2>Appliances for your home | Up to 55% off</h2>
                     <div className="category-items">
                         <div className="category-item">
-                            <img src="D:\Study Materials\shopping (2).webp" />
-                            <p>Air conditioners</p>
-                        </div>
-                        <div className="category-item">
-                            <img src="D:\Study Materials\in-ref-convertible-freezer-rf57a5032s9-tl-436322271.avif" />
-                            <p>Refrigerators</p>
-                        </div>
-                        <div className="category-item">
-                            <img src="D:\Study Materials\shopping (3).webp" />
-                            <p>Microwaves</p>
-                        </div>
-                        <div className="category-item">
-                            <img src="D:\Study Materials\in-front-loading-washer-ww10t534daws1-374541-374541-ww80t504dax1tl-538070792.webp" />
-=======
-                <div class="category">
-                    <h2>Appliances for your home | Up to 55% off</h2>
-                    <div class="category-items">
-                        <div class="category-item">
                             <img src="https://img3.exportersindia.com/product_images/bc-full/dir_153/4571808/room-wall-air-conditioner-1497420516-3063967.jpeg" />
                             <p>Air conditioners</p>
                         </div>
-                        <div class="category-item">
+                        <div className="category-item">
                             <img src="https://images.samsung.com/is/image/samsung/p6pim/in/rf57a5032s9-tl/gallery/in-ref-convertible-freezer-rf57a5032s9-tl-436322271?$1300_1038_PNG$" />
                             <p>Refrigerators</p>
                         </div>
-                        <div class="category-item">
+                        <div className="category-item">
                             <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTWJxtqY1uoBI-fC71YXU5ZV58xad_hgndnk-_4CK36zz2R018n_BXIWJtz0E2O88XWACYIg-xB0a7pOemXbw4in-eEqH7DNwznbPr9uRcEctoSzoBQyg9-&usqp=CAEp" />
                             <p>Microwaves</p>
                         </div>
-                        <div class="category-item">
+                        <div className="category-item">
                             <img src="https://images.samsung.com/is/image/samsung/p6pim/in/ww80t504dax1tl/gallery/in-front-loading-washer-ww10t534daws1-374541-374541-ww80t504dax1tl-538070792?$1300_1038_PNG$" />
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
                             <p>Washing machines</p>
                         </div>
                     </div>
                     <button>See more</button>
                 </div>
 
-<<<<<<< HEAD
                 <div className="category">
-=======
-                <div class="category">
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
                     <h2>Sign in for your best experience</h2>
                     <button>Sign in securely</button>
                     
@@ -220,11 +141,7 @@ function App() {
         </section>
     </main>
     <footer>
-<<<<<<< HEAD
         <div className="container">
-=======
-        <div class="container">
->>>>>>> 1d3dc73581e0315a24fdee3aad39ae3c7b945baa
             <p>&copy; 2024 harikrishnans.com, Inc. or its affiliates</p>
         </div>
     </footer>
---
+++ ecommerce/src/componets/signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <p>Sign in to your account!</p>
+    </div>
+  );
+}
+
+export default SignIn;
